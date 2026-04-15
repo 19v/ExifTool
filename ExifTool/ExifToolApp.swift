1214acc466c04b807c50767a6400a9d881f862fd
19v <jhc_98219@live.com>
2026-04-15 14:44:28 +0800

    Start commit

---
+++ ExifTool/ExifToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExifToolApp.swift
+//  ExifTool
+//
+//  Created by Haochen on 2026/4/12.
+//
+
+import SwiftUI
+
+@main
+struct ExifToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
