e5bb222276a30403fbd0ae3904651fbc1bde60c2
19v <jhc_98219@live.com>
2026-08-18 18:37:07 +0800

    加宽图库导航标题背景

--- ExifTool/Views/iOS/IOSLibraryBrowserView.swift
+++ ExifTool/Views/iOS/IOSLibraryBrowserView.swift
@@ -344,7 +344,7 @@ private struct LibraryNavigationTitle: View {
             .lineLimit(1)
             .truncationMode(.tail)
             .padding(.horizontal, 12)
-            .frame(height: 44)
+            .frame(width: 120, height: 44)
             .platformGlassBackground(in: Capsule())
     }
 }
